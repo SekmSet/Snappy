ea309fb8ee5230efa240d539cbcc92e6ac5e083d
Jordan <jordan.philippot@epitech.eu>
2020-06-01 19:40:15 +0200

    footer update

--- front-end/src/components/parts/footer.js
+++ front-end/src/components/parts/footer.js
@@ -5,11 +5,7 @@ const Footer = () => {
   return (
     <footer className="text-muted">
       <div className="container">
-<<<<<<< HEAD
         <p> © Snappy app By Les Pains Durs !</p>
-=======
-        <p> © My Snapchat with React, NodeJS and Mongo By SekhmSet, Belicuss, Jojo & Tchointeur !</p>
->>>>>>> auth login register react native
       </div>
     </footer>
   );
